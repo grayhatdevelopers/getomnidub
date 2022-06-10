4a5734587068489c67d3ae19c790792aac44b5ea
Saad Bazaz <saadbazaz@hotmail.com>
2022-06-11 00:00:05 +0500

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./components/FullScreenVideo/fullscreenvideo.css";
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/signin';
+import NotFound from './pages/notfound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path='/' component={Home} exact />
         <Route path='/signin' component={SigninPage} exact />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
