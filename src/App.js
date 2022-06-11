ac2c14d6cebebec3e4b8a7b47bc3a9e6f93d4073
Saad Bazaz <saadbazaz@hotmail.com>
2022-06-11 13:47:42 +0500

    Add metadata and favicon

--- src/App.js
+++ src/App.js
@@ -10,6 +10,16 @@ import NotFound from './pages/notfound';
 function App() {
   return (
     <Router>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="description" content="Automatically dub videos into other languages."/>
+        <meta name="og:title" content="DeepDub | Videos for everyone"/>
+        <meta name="og:description" content="Automatically dub videos into other languages"/>
+        <meta name="og:image" content="../public/thumbnail.png"/>
+      </head>
       <Switch>
         <Route path='/' component={Home} exact />
         <Route path='/signin' component={SigninPage} exact />
