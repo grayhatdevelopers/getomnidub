a564a50135d8479793113f0ec0c7ae2a7ea48a14
SaadBazaz <SaadBazaz@users.noreply.github.com>
2022-11-08 09:10:38 +0000

    deploy: 700fe9defb23fedf7edaeb5abb955baab5bdece9

---
+++ precache-manifest.52508f89f842da56817f7949d2341d11.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2aa8d38a9ff48e44ed483a44ebfb0be",
+    "url": "/getomnidub/index.html"
+  },
+  {
+    "revision": "490b3647a91742815979",
+    "url": "/getomnidub/static/css/main.9c2dcef4.chunk.css"
+  },
+  {
+    "revision": "552e249f2d8754632ee2",
+    "url": "/getomnidub/static/js/2.2fd08e15.chunk.js"
+  },
+  {
+    "revision": "286a64e2d3c03eda1bde4615039439bd",
+    "url": "/getomnidub/static/js/2.2fd08e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490b3647a91742815979",
+    "url": "/getomnidub/static/js/main.98838e13.chunk.js"
+  },
+  {
+    "revision": "f5f40ea1fb59235bd672",
+    "url": "/getomnidub/static/js/runtime-main.bcf5e0f7.js"
+  },
+  {
+    "revision": "e4901b9971ce310e49ea29d51b3b2f76",
+    "url": "/getomnidub/static/media/calling.e4901b99.png"
+  },
+  {
+    "revision": "7df97b1cd5f3662154185e39df29351a",
+    "url": "/getomnidub/static/media/dubbing.7df97b1c.png"
+  },
+  {
+    "revision": "be01e1d41ec8cd1094dcc578aa7ac45e",
+    "url": "/getomnidub/static/media/film-icon.be01e1d4.jpeg"
+  },
+  {
+    "revision": "5f467cfe04881ddb728f44830e19ac0f",
+    "url": "/getomnidub/static/media/multilingual.5f467cfe.png"
+  },
+  {
+    "revision": "7e571a7947393571e2dab7ea39b62b99",
+    "url": "/getomnidub/static/media/people.7e571a79.png"
+  },
+  {
+    "revision": "620aaf437d90d97b12b63f36242f2295",
+    "url": "/getomnidub/static/media/tv-anchor.620aaf43.webp"
+  },
+  {
+    "revision": "f9b80de400808aea67a7c9076691fada",
+    "url": "/getomnidub/static/media/video.f9b80de4.mp4"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/getomnidub/precache-manifest.52508f89f842da56817f7949d2341d11.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/getomnidub/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
